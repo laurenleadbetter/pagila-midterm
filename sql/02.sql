27af05b0c5fd3c3d69b6a60ac0fbb70e93d70b0c
Lauren Leadbetter <lauren.leadbetter.23@cmc.edu>
2023-03-19 10:57:48 -0700

    added amswers

--- sql/02.sql
+++ sql/02.sql
@@ -1,4 +1,15 @@
 /*
  * Write a SQL query SELECT query that:
  * computes the country with the most customers in it. 
+ *
  */
+ 
+
+SELECT country FROM country 
+JOIN address USING (city_id) 
+JOIN city USING (country_id) 
+JOIN customer USING (address_id) 
+
+GROUP BY country 
+ORDER BY count(customer_id) DESC
+LIMIT 1;
--- sql/03.sql
+++ sql/03.sql
@@ -3,3 +3,24 @@
  * List the first and last names of all actors who have appeared in movies in the "Children" category,
  * but that have never appeared in movies in the "Horror" category.
  */
+
+SELECT DISTINCT first_name, last_name 
+FROM actor
+WHERE actor.actor_id in
+    (SELECT actor_id
+        FROM actor
+        JOIN film_actor using (actor_id)
+        JOIN film using (film_id)
+        JOIN film_category using (film_id)
+        JOIN category using (category_id)
+        WHERE name = 'Children' 
+
+   AND actor.actor_id not in
+    (SELECT DISTINCT actor_id
+        FROM actor
+        JOIN film_actor using (actor_id)
+        JOIN film using (film_id)
+        JOIN film_category using (film_id)
+        JOIN category using (category_id)
+        WHERE name = 'Horror')
+ORDER BY last_name ASC; 
